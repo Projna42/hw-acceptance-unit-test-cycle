b1e83c6b82174a606e8e19624fc58c95696eecfa
EC2 Default User <ec2-user@ip-172-31-8-237.us-east-2.compute.internal>
2019-03-07 07:07:42 +0000

    <your message>

--- features/step_definitions/movies_steps.rb
+++ features/step_definitions/movies_steps.rb
@@ -1,25 +1,15 @@
 
 Given /the following movies exist/ do |movies_table|
-  movies_table.hashes.each do |movie|
-    Movie.create movie
-  end
+    movies_table.hashes.each do |movie|
+        Movie.create!(movie)
+    end
 end
 
-Then /I should see "(.*)" before "(.*)"/ do |e1, e2|
-  #  ensure that that e1 occurs before e2.
-  #  page.body is the entire content of the page as a string.
-  expect(page.body.index(e1) < page.body.index(e2))
+Then /the director of "([^"]*)" should be "([^"]*)"/ do |movie, director|
+    step %Q{I should see "#{movie}"}
+    step %Q{I should see "#{director}"}
 end
 
-When /I (un)?check the following ratings: (.*)/ do |uncheck, rating_list|
-  rating_list.split(', ').each do |rating|
-    step %{I #{uncheck.nil? ? '' : 'un'}check "ratings_#{rating}"}
-  end
-end
-
-Then /I should see all the movies/ do
-  # Make sure that all the movies in the app are visible in the table
-  Movie.all.each do |movie|
-    step %{I should see "#{movie.title}"}
-  end
+Then /I should see "([^"]*)" before "([^"]*)"/ do |movieA, movieB|
+    expect(page.body).to match(/#{movieA}.*#{movieB}/m)
 end
